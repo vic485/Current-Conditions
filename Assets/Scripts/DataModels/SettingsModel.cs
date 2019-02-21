4a945cd543c5f780ff9d7583c1e216fb7c4dff48
Victor Fugazzotto <victorfuga@gmail.com>
2019-02-21 13:10:19 -0500

    Some small changes

--- Assets/Scripts/DataModels/SettingsModel.cs
+++ Assets/Scripts/DataModels/SettingsModel.cs
@@ -4,6 +4,8 @@
     {
         public string ApiKey { get; set; } = string.Empty;
         public int WaitMin { get; set; } = 15;
+        public bool AutoLocation { get; set; }
+        public string City { get; set; }
     }
 
 }
--- Assets/Scripts/Management/SystemManager.cs
+++ Assets/Scripts/Management/SystemManager.cs
@@ -10,46 +10,7 @@ namespace Management
 {
     public class SystemManager : MonoBehaviour
     {
-        [SerializeField] private CurrentConditionsHelper currentConditions;
-
-        private float _timer = 0f;
-        private int _waitTime;
-        private WeatherClient _weatherClient;
-
-        private void Start()
-        {
-            LoadOrCreateSettings();
-            UpdateUi();
-        }
-
-        private void Update()
-        {
-            _timer += Time.deltaTime;
-
-            if (!(_timer >= _waitTime))
-                return;
-
-            UpdateUi();
-        }
-
-        private void UpdateUi()
-        {
-            _timer = 0f;
-            currentConditions.ChangeData(_weatherClient.GetWeather());
-        }
-
-        private void LoadOrCreateSettings()
-        {
-            var path = Path.Combine(Application.persistentDataPath, "config.json");
-            
-            if (!File.Exists(path))
-                File.WriteAllText(path, JsonConvert.SerializeObject(new SettingsModel(), Formatting.Indented));
-
-            var settings = JsonConvert.DeserializeObject<SettingsModel>(File.ReadAllText(path));
-            
-            _weatherClient = new WeatherClient(settings.ApiKey);
-            _waitTime = settings.WaitMin * 60;
-        }
+        internal static SettingsModel Settings;
     }
 
 }
