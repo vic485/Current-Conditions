eb6b5808f67798aa79017b85fb20e696974cdbc4
Victor Fugazzotto <victorfuga@gmail.com>
2020-08-19 09:38:53 -0400

    Settings I think?

---
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+internal static class SettingsManager
+{
+    private static readonly string FilePath = Path.Combine(Application.persistentDataPath, "settings.json");
+
+    internal static Settings Settings;
+
+    static SettingsManager()
+    {
+        if (!File.Exists(FilePath))
+            File.WriteAllText(FilePath, JsonConvert.SerializeObject(new Settings(), Formatting.Indented));
+
+        Settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(FilePath));
+    }
+}
+
+internal class Settings
+{
+    /// <summary>
+    /// OpenWeatherMap api key
+    /// </summary>
+    public string ApiKey { get; set; }
+    
+    /// <summary>
+    /// Time to wait (in seconds) before re-checking weather
+    /// </summary>
+    public int TimeDelay { get; set; } = 600;
+}
---
+++ Assets/Scripts/Managers/WeatherManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using OpenWeatherAPI;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    private API _apiClient;
+    private float _timer;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _apiClient = new API(/*SettingsManager.Settings.ApiKey*/ "");
+
+        try
+        {
+            var weather = _apiClient.Query("4780641");
+            Debug.Log(weather.Name);
+        }
+        catch (WebException e)
+        {
+            if (e.Status == WebExceptionStatus.ProtocolError) 
+            {
+                if (((HttpWebResponse) e.Response).StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    // TODO: Show pop-up to check api key
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer < SettingsManager.Settings.TimeDelay)
+            return;
+
+        _timer = 0;
+        // TODO: re-poll weather
+    }
+}
